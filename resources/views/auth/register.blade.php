04de09038dc650c77acd705e6684d53655a5286b
unknown <admin@MAC-IT.avendomain.local>
2021-12-28 08:57:34 +0800

    Updated registration.blade.php

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -89,7 +89,7 @@
           <div class="row">
             <div class="col-8">
               <div class="icheck-primary">
-                <input type="checkbox" id="agreeTerms" name="terms" value="agree">
+                <input type="checkbox" checked id="agreeTerms" name="terms" value="agree">
                 <label for="agreeTerms">
                  I agree to the <a href="#">terms</a>
                 </label>
