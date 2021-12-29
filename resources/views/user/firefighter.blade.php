150303622d28d80ed13d991e9ccc995b6c95a2fe
unknown <admin@MAC-IT.avendomain.local>
2021-12-29 14:18:08 +0800

    Update MapBox

--- resources/views/user/firefighter.blade.php
+++ resources/views/user/firefighter.blade.php
@@ -27,12 +27,24 @@
     .message-holder {
         display: flex;
         flex-direction: column;
-        justify-content: flex-end;
+        justify-content: center;
+        align-items: center;
+        height: 100%;
     }
     .btn-cta {
-        border: 1px dashed red;
-        padding: 10px 0;
+        /* border: 1px dashed red; */
+        padding: 15px 20px;
         display: inline-block;
+        background: crimson;
+        color: #fff;
+        border-radius: 24px;
+        text-decoration: none !important;
+        box-shadow: 0 10px 15px rgba(0, 0, 0, 0.2);
+        transition: .3s;
+    }
+    .btn-cta:hover {
+        color: #fff !important;
+        padding: 20px 25px;
     }
     #map {
         border: 8px solid #fff;
@@ -42,9 +54,9 @@
 </style>
 
 @push('mapbox-style')
-<script src='https://api.mapbox.com/mapbox-gl-js/v2.3.1/mapbox-gl.js'></script>
+<link rel="stylesheet" href="https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-directions/v4.1.0/mapbox-gl-directions.css" type="text/css">
+
 <link href='https://api.mapbox.com/mapbox-gl-js/v2.3.1/mapbox-gl.css' rel='stylesheet' />
-<script src="https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-directions/v4.1.0/mapbox-gl-directions.js"></script>
 
 @endpush
 
@@ -58,20 +70,18 @@
                 </div>
                 <div class="detail-holder ml-4">
                     <h3>Pulilan Fire Fighter</h3>
-                    <strong>0922-222-2222</strong><br>
                     <strong>0922-222-2222</strong>
                     <p class="text-muted mb-0">Brgy. Puchuchu, Pulilan Bulacan</p>
                 </div>
             </div>
             <div class="col-lg-6">
                <div class="message-holder">
-                   <a href="#" class="btn-cta">Make a phone call</a>
-                   <a href="#" class="btn-cta">Call to messenger</a>
+                   <a href="tel:09222222222" class="btn-cta phone">Make a phone call</a>
                </div>
             </div>
         </div>
     </div>
-<div id='map' class="mt-5 mb-5"></div>
+<div id='map' class="mt-1 mb-5"></div>
 
 </div>
 
@@ -79,19 +89,22 @@
 @endsection
 
 @push('mapbox-script')
-<link rel="stylesheet" href="https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-directions/v4.1.0/mapbox-gl-directions.css" type="text/css">
+<script src='https://api.mapbox.com/mapbox-gl-js/v2.3.1/mapbox-gl.js'></script>
+<script src="https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-directions/v4.1.0/mapbox-gl-directions.js"></script>
+
 <script>
     mapboxgl.accessToken = 'pk.eyJ1IjoibWFya2FuMm55IiwiYSI6ImNreHF3aGhzMTU4a3Qyd3BmanUwbmh4anIifQ.aY6MrSmJZlwu2HGMUp0JSQ';
 
     navigator.geolocation.getCurrentPosition(successLocation, errorLocation, { enableHighAccuracy: true });
 
+    setupMap();
 
     function successLocation(position) {
         setupMap([ position.coords.longitude, position.coords.latitude]);
     }
 
     function errorLocation() {
-        setupMap([120.8458, 14.9124]);
+        setupMap([120.865291, 14.9020627]);
     }
 
     function setupMap(center) {
@@ -99,7 +112,7 @@
             container: 'map',
             style: 'mapbox://styles/mapbox/streets-v11',
             center: center,
-            zoom: 15
+            zoom: 16,
         });
 
         //Navigator
@@ -113,6 +126,18 @@
         // profile: 'mapbox/cycling'
         });
         map.addControl(directions, 'top-left');
+
+        map.addControl(
+            new mapboxgl.GeolocateControl({
+                positionOptions: {
+                enableHighAccuracy: true
+                },
+                // When active the map will receive updates to the device's location as it changes.
+                trackUserLocation: true,
+                // Draw an arrow next to the location dot to indicate which direction the device is heading.
+                showUserHeading: true
+                })
+        );
 
     }
 
