f2a3118ec723809940027425fe03ed8242fb78db
unknown <git.markan2nypascual@gmail.com>
2022-01-31 09:02:22 +0800

    Updated RegisterController

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -64,7 +64,7 @@ class RegisterController extends Controller
             'address' => ['string', 'max:255'],
             'phone' => ['required', 'string', 'digits:11'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'password' => ['required', 'string', 'min:8'],
         ]);
     }
 
